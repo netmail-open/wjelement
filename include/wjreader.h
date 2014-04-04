36648fa05935ccfe005179d2cff2d5e95cfb4068
petehug <pete@kapiti.co.nz>
2014-04-04 14:41:51 +1300

    WJE_DISTINGUISH_INTEGER_TYPE related fix

--- include/wjreader.h
+++ include/wjreader.h
@@ -83,9 +83,7 @@ typedef enum {
 	WJR_TYPE_STRING			= 'S',
 	WJR_TYPE_NUMBER			= 'N',
 
-#ifndef WJE_DISTINGUISH_INTEGER_TYPE
-	WJR_TYPE_INTEGER		= 'N',
-#else
+#ifdef WJE_DISTINGUISH_INTEGER_TYPE
 	WJR_TYPE_INTEGER		= 'I',
 #endif
 
