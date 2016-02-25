0ea9e59553f20d9f3293774460d867654935fca5
Owen Swerkstrom <owenswerk@gmail.com>
2016-02-25 16:39:05 -0600

    tidy/revert oneOf

--- src/wjelement/schema.c
+++ src/wjelement/schema.c
@@ -699,8 +699,8 @@ static XplBool SchemaValidate(WJElement schema, WJElement document,
 				if (num == 0) {
 					fail = TRUE;
 					if (err) {
-						err(client, 
-							"%s failed anyOf validation.", 
+						err(client,
+							"%s failed anyOf validation.",
 							document->name);
 					}
 				}
@@ -710,35 +710,25 @@ static XplBool SchemaValidate(WJElement schema, WJElement document,
 		} else if (!stricmp(memb->name, "oneOf")) {
 			if (document && memb->type == WJR_TYPE_ARRAY) {
 				num = 0;
-				val = 0;
-				arr = NULL;
-				while ((arr = WJEGet(memb, "[]", arr))) {
-					data = NULL;
-					if (val < memb->count) {
-						MemAsprintf(&str, "[%d]", val);
-						data = WJEGet(memb, str, NULL);
-						MemFree(str);
-					}
-					MemAsprintf(&str, "%s[%d]", name, val);
-					if (SchemaValidate(data, document, err, loadcb, freecb,
-						client, str, version)) {
-						num++;
-					}
-					MemFree(str);
-					str = NULL;
-					val++;
-				}
-				if (num != 1) {
-					fail = TRUE;
-					if (err) {
-						err(client, 
-							"%s failed oneOf validation; matched %d.", 
-							document->name, 
-							num);
+				fail = TRUE;
+				while((arr = WJEGet(memb, "[]", arr))) {
+					if(SchemaValidate(arr, document, err, loadcb, freecb, client, name, version)) {
+						++num;
+						if(num > 1) {
+							break;
+						}
 					}
 				}
+				if(num == 1) {
+					fail = FALSE;
+				}
+				else {
+					if(err) {
+						err(client, "%s did not match exactly one specified schema", name);
+					}
+				}
+				anyFail = anyFail || fail;
 			}
-			anyFail = anyFail || fail;
 
 		} else if (!stricmp(memb->name, "allOf")) {
 			if (document && memb->type == WJR_TYPE_ARRAY) {
@@ -764,10 +754,10 @@ static XplBool SchemaValidate(WJElement schema, WJElement document,
 				if (num != memb->count) {
 					fail = TRUE;
 					if (err) {
-						err(client, 
+						err(client,
 							"%s failed allOf validation; matches: %d, expected: %d", 
-							document->name, 
-							num, 
+							document->name,
+							num,
 							memb->count);
 					}
 				}
