bf4909d0e1709120bac361dd5a13103cee48fb13
Hugo Klepsch <hklepsch@carnegietechnologies.com>
2017-11-14 11:29:44 -0500

    Rename min/max macros to fix stdlib naming clash
    * Addresses issue #49
    * Renamed to xpl_min and xpl_max

--- include/xpl.h
+++ include/xpl.h
@@ -55,12 +55,12 @@
 
 /* xpldefs.h */
 
-#ifndef min
-#define min(a, b)   (((a) < (b))? (a): (b))
+#ifndef xpl_min
+#define xpl_min(a, b)   (((a) < (b))? (a): (b))
 #endif
 
-#ifndef max
-#define max(a, b)   (((a) > (b))? (a): (b))
+#ifndef xpl_max
+#define xpl_max(a, b)   (((a) > (b))? (a): (b))
 #endif
 
 
